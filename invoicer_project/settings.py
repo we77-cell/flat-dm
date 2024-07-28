627129214b0031a414b613fa4f956d191c64ff9d
we77-cell <amzv02@gmail.com>
2024-07-28 11:15:49 +0000

    Update settings.py and setting the new secret key

--- invoicer_project/settings.py
+++ invoicer_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$lv^i8v3n0-+6me6v)er+==&axqngojo_3+7al-d0fmm0$=d&c'
+SECRET_KEY = os.environ_get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
